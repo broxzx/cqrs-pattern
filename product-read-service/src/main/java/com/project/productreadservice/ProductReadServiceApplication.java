affc836e9ba18ff41b6e0dd9814c431cd3256d49
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-08-23 15:55:36 +0200

    added main modules

---
+++ product-read-service/src/main/java/com/project/productreadservice/ProductReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.productreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductReadServiceApplication.class, args);
+    }
+
+}
---
+++ product-write-service/src/main/java/com/project/productwriteservice/ProductWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.productwriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWriteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductWriteServiceApplication.class, args);
+    }
+
+}
